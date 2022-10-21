6ec91103f2be03e3e831f5e253b67567e6d54fb5
freyjabun <freyjabun@gmail.com>
2022-10-21 14:56:04 +0200

    first commit

---
+++ Alice.java
@@ -0,0 +1,115 @@
+import java.net.*;
+import java.nio.file.Files;
+import java.io.*;
+import java.security.*;
+import java.security.spec.*;
+
+import javax.crypto.Cipher;
+
+public class Alice {
+    private Socket aliceSocket;
+    private OutputStream out;
+    private InputStream in;
+
+    static PublicKey alicePublic;
+    static PrivateKey alicePrivate;
+    static PublicKey bobKey;
+
+    static int g = 666;
+    static int h = 420;
+    static int p = 6661;
+    static int messageLength = 256;
+    
+
+    public void startConnection(String ip, int port) {
+        try {
+            aliceSocket = new Socket(ip, port);
+            out = aliceSocket.getOutputStream();
+            in = aliceSocket.getInputStream();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public byte[] sendMessage(String msg) {
+        try {
+            out.write(RSAEncrypt(msg));
+            System.out.println(msg);
+            byte[] resp = in.readNBytes(messageLength); //blocking
+            System.out.println(resp);
+            return resp;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void stopConnection() {
+        try {
+            in.close();
+            out.close();
+            aliceSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    public static KeyPair generateKeyPair(){
+        try {
+            KeyPairGenerator gen = KeyPairGenerator.getInstance("RSA");
+            gen.initialize(2048);
+            KeyPair pair = gen.generateKeyPair();
+            return pair;
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public byte[] RSAEncrypt(String s){
+        try {
+            Cipher encryptCipher = Cipher.getInstance("RSA");
+            encryptCipher.init(Cipher.ENCRYPT_MODE, bobKey); //Change to bob key when work hehe
+            return encryptCipher.doFinal(s.getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void run(){
+
+        KeyPair pair = generateKeyPair();
+        alicePublic = pair.getPublic();
+        alicePrivate = pair.getPrivate();
+        
+        try (FileOutputStream fos = new FileOutputStream("alice.key")) {
+            fos.write(alicePublic.getEncoded());
+            
+            System.out.println("Alice key file generated");
+            out.write(1);
+            System.out.println("alice key entered output stream");
+            in.read();
+            System.out.println("I can read Bob");
+
+            File publicKeyFile = new File("boblic.key");
+            byte[] publicKeyBytes = Files.readAllBytes(publicKeyFile.toPath());
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(publicKeyBytes);
+            bobKey = keyFactory.generatePublic(publicKeySpec);
+
+            System.out.println("Bob file read");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+    public static void main(String[] args) {
+        Alice client = new Alice();
+        client.startConnection("127.0.0.1", 6666);
+        client.run();
+        client.sendMessage("hello server");
+    }
+}
---
+++ Bob.java
@@ -0,0 +1,105 @@
+import java.net.*;
+import java.nio.file.Files;
+import java.io.*;
+import java.security.*;
+import java.security.spec.EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+
+import javax.crypto.Cipher;
+
+public class Bob {
+    private ServerSocket serverSocket;
+    private Socket bobSocket;
+    private OutputStream out;
+    private InputStream in;
+
+    static int g = 666;
+    static int h = 420;
+    static int p = 6661;
+    static int messageLength = 256;
+
+    static PublicKey Boblic;
+    static PrivateKey Brivate;
+    static PublicKey aliceKey;
+
+    public void start(int port) {
+        try {
+            serverSocket = new ServerSocket(port);
+            bobSocket = serverSocket.accept();
+            out = bobSocket.getOutputStream();
+            in = bobSocket.getInputStream();
+        
+    } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public void stop() {
+        try {
+            in.close();
+            out.close();
+            bobSocket.close();
+            serverSocket.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public static KeyPair generateKeyPair(){
+        try {
+            KeyPairGenerator gen = KeyPairGenerator.getInstance("RSA");
+            gen.initialize(2048);
+            KeyPair pair = gen.generateKeyPair();
+            return pair;
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public byte[] RSAEncrypt(String s){
+        try {
+            Cipher encryptCipher = Cipher.getInstance("RSA");
+            encryptCipher.init(Cipher.ENCRYPT_MODE, aliceKey); //Change to bob key when work hehe
+            return encryptCipher.doFinal(s.getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void run(){
+
+        KeyPair pair = generateKeyPair();
+        Boblic = pair.getPublic();
+        Brivate = pair.getPrivate();
+
+        try (FileOutputStream fos = new FileOutputStream("boblic.key")) {
+            fos.write(Boblic.getEncoded());
+            System.out.println("Made boblic file");
+            
+            out.write(1);
+            System.out.println("Boblic stuff entered output stream");
+            in.read();
+            System.out.println("I can read Alice");
+
+            File publicKeyFile = new File("alice.key");
+            byte[] publicKeyBytes = Files.readAllBytes(publicKeyFile.toPath());
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(publicKeyBytes);
+            aliceKey = keyFactory.generatePublic(publicKeySpec);
+            System.out.println("Alice file read");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    
+    public static void main(String[] args) {
+        Bob bob = new Bob();
+        bob.start(6666);
+        bob.run();
+    }
+}
